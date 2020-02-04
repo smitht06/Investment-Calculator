f5a7f88b0c9611852cca6af66f207f87c3e99c01
Anthony Smith <as361@students.uwf.edu>
2020-02-04 14:00:12 -0500

    IO

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Object[] assets = new Object[18];
-        Object[][] assets2 = new Object[18][5];
+        String[] assets = new String[18];
+        String[][] assets2 = new String[18][5];
         try{
             FileInputStream fileStream = null;
             Scanner fileIn = null;
@@ -21,8 +22,17 @@ public class Main {
             }
 
             for(int i = 0; i < assets.length-1; i++){
-                    System.out.println(assets[i]);
-                }
+                for(int j = 0; j < assets.length-1; j++){
+                    assets2[i] = assets[i].split(",");
+            }}
+
+
+//            assets2[0] = assets[0].split(",");
+            for(int i = 0; i < assets.length-1; i++){
+                System.out.println(Arrays.toString(assets2[i]));
+            }
+
+
 
         }catch(FileNotFoundException e){
             System.out.println("File not found.");
