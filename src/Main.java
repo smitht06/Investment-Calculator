336c24ea7e84561bd2b91f2db4a2e6f46cc15fe1
Anthony Smith <as361@students.uwf.edu>
2020-02-06 10:37:39 -0500

    created stable asset list

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,21 @@ public class Main {
         BufferedReader fileRead = null;
         try{
             fileRead = new BufferedReader(new FileReader("assetData.csv"));
-            List<Stocks> stocks = new ArrayList<>();
+            List<Stocks> stockList = new ArrayList<>();
+            List<StableAssets> stableAssetsList = new ArrayList<>();
             String line = "";
             fileRead.readLine();
             while ((line = fileRead.readLine()) != null){
                 String[] assetDetails = line.split(",");
                 if(assetDetails.length > 3){
                     Stocks stk = new Stocks(assetDetails[0],assetDetails[1],Double.parseDouble(assetDetails[2]),Double.parseDouble(assetDetails[3]),Double.parseDouble(assetDetails[4]));
-                    stocks.add(stk);
+                    stockList.add(stk);
                 }else if (assetDetails.length < 4){
                     StableAssets stb = new StableAssets(assetDetails[0],assetDetails[1],Double.parseDouble(assetDetails[2]));
+                    stableAssetsList.add(stb);
                 }
             }
-            for(Stocks e : stocks){
+            for(Stocks e : stockList){
                 System.out.println(e.getName());
                 System.out.println(e.getID());
                 System.out.println(e.getExpected1YearReturn());
