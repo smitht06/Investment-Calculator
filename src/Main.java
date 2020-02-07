13797204f43f2cd82f9685a4fa266c810840494c
Anthony Smith <as361@students.uwf.edu>
2020-02-07 14:18:34 -0500

    add objects to list

--- src/Main.java
+++ src/Main.java
@@ -9,25 +9,24 @@ public class Main {
         BufferedReader fileRead = null;
         try{
             fileRead = new BufferedReader(new FileReader("assetData.csv"));
-            List<Stocks> stockList = new ArrayList<>();
-            List<StableAssets> stableAssetsList = new ArrayList<>();
+            List<Asset> assetList = new ArrayList<>();
+
             String line = "";
             fileRead.readLine();
             String[] assetDetails = new String[17];
             String[][] assetDetails2d = new String[17][5];
             int i = 0;
             while ((line = fileRead.readLine()) != null) {
+                int j = 0;
                 assetDetails2d[i] = line.split(",");
                 i++;
-
-            }
-
-
-            for(int j = 0; j < 17; j++){
-                for(int k = 0; k < 5; k++){
-                    System.out.println(assetDetails2d[j][k]);
+                if(assetDetails2d[i].length > 3){
+                    Asset a = new Stocks(assetDetails2d[i][0],assetDetails2d[i][1],Double.parseDouble(assetDetails2d[i][2]),Double.parseDouble(assetDetails2d[i][3]),Double.parseDouble(assetDetails2d[i][4]));
+                    assetList.add(a);
                 }
+
             }
+
             }
         catch(FileNotFoundException e){
             System.out.println("File not found.");
