f15158854d24991dafb2f6165bb0b57e56baef27
Anthony Smith <as361@students.uwf.edu>
2020-02-12 12:05:01 -0500

    created table print method

--- src/Main.java
+++ src/Main.java
@@ -15,16 +15,8 @@ public class Main {
             }
             System.out.println("Enter the number of the asset you would like to invest in: ");
             try {
-                Scanner sc = new Scanner(System.in);
-                choice = sc.nextInt();
-            }catch (InputMismatchException e){
-                System.out.println("Invalid, Enter a number between 1 an 17 or enter -1 to exit");
-            }
-            if(choice > 17){
-                System.out.println("Invalid choice, please enter a number from the list or enter negative number to exit.");
-            }else if (choice < 0){
-                break;
-            }
+            Scanner sc = new Scanner(System.in);
+            choice = sc.nextInt();
             System.out.println("Enter amount to invest in " + masterList.get(choice-1).getName());
             Scanner sc2 = new Scanner(System.in);
             amountInvested = sc2.nextInt();
@@ -32,25 +24,41 @@ public class Main {
             int profit = currentAsset.valueInTenYears(amountInvested);
             System.out.println("Investment value after 10 years is: $" + profit);
             System.out.println("Press enter to continue: ");
-
-
             currentAsset.setAmountInvested(amountInvested);
             currentAsset.setProfit(profit);
             portfolio.add(currentAsset);
             Scanner sc3 = new Scanner(System.in);
-            String pressEnter = sc3.nextLine();
-
-
-
-
+            int continueOrExit = sc3.nextInt();
+            if (continueOrExit <= 0){
+                break;
+            }
+            }catch (InputMismatchException e){
+                System.out.println("Invalid, Enter a number between 1 an 17 or enter -1 to exit");
+                continue;
+            } catch (IndexOutOfBoundsException e){
+                System.out.println("Invalid, Enter a number between 1 an 17 or enter -1 to exit");
+            }if (choice < 0){
+                break;
+            }
         }
+        printTable(portfolio);
+
+    }
+    public static void printTable(ArrayList<Asset> portfolio){
         System.out.println("+--------------+-----------------+--------------------+");
         System.out.println("| ASSET SYMBOL | AMOUNT INVESTED | VALUE IN TEN YEARS |");
+        System.out.println("+==============+=================+====================+");
+        int totalInvest = 0;
+        int totalProfit = 0;
+        String total = "TOTAL";
         for (Asset asset : portfolio) {
-            System.out.println(asset.getName() + asset.getAmountInvested() + asset.getProfit());
+            System.out.printf("| %-13.13s| %-16d| %-19d|\n",asset.getName(),asset.getAmountInvested(),asset.getProfit());
+            totalInvest += asset.getAmountInvested();
+            totalProfit += asset.getProfit();
         }
-
-
+        System.out.println("+--------------+-----------------+--------------------+");
+        System.out.printf("| %-13.13s| %-16d| %-19d|\n",total,totalInvest,totalProfit);
+        System.out.println("+--------------+-----------------+--------------------+");
     }
 
     public static ArrayList<Asset> readFile(String fileName){
--- src/Stocks.java
+++ src/Stocks.java
@@ -20,29 +20,7 @@ public class Stocks extends Asset{
         this.expected90DayReturn = 0;
     }
 
-    public double getExpected5YearReturn() {
-        return expected5YearReturn;
-    }
 
-    public void setExpected5YearReturn(double expected5YearReturn) {
-        this.expected5YearReturn = expected5YearReturn;
-    }
-
-    public double getExpected1YearReturn() {
-        return expected1YearReturn;
-    }
-
-    public void setExpected1YearReturn(double expected1YearReturn) {
-        this.expected1YearReturn = expected1YearReturn;
-    }
-
-    public double getExpected90DayReturn() {
-        return expected90DayReturn;
-    }
-
-    public void setExpected90DayReturn(double expected90DayReturn) {
-        this.expected90DayReturn = expected90DayReturn;
-    }
 
 
 
